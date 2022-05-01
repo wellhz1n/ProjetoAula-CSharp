fc8f394658c354664dd4409d018a6bc35450b2e1
Wellhz1n <wellingtom1234520@gmail.com>
2022-05-01 18:05:20 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoAulaEdy/Classes/Usuario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAulaEdy.Classes
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+
+        public string Email { get; set; }
+
+
+
+
+        public Usuario()
+        {
+        }
+        public bool ValidaCpf()
+        {
+            return Cpf.Length == 11;
+        }
+
+        public bool VerificarNome()
+        {
+            return !string.IsNullOrEmpty(Nome);
+        }
+
+        public bool EmailValido()
+        {
+            return !string.IsNullOrEmpty(Email) && Email.Contains("@");
+        }
+
+
+    }
+}
---
+++ ProjetoAulaEdy/Program.cs
@@ -0,0 +1,91 @@
+﻿using ProjetoAulaEdy.Classes;
+using System;
+using System.Collections.Generic;
+
+namespace ProjetoAulaEdy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool rodando = true;
+            int menu = 0;
+            while (rodando)
+            {
+
+            menu = getMenu();
+
+            Console.Clear();
+
+            if (menu == 1)
+            {
+
+                Usuario user = new Usuario();
+
+                Console.WriteLine("Digite o seu nome:");
+                user.Nome = Console.ReadLine();
+
+                Console.WriteLine($" Seu nome é : {user.Nome}");
+
+                Console.WriteLine("Digite o seu cpf:");
+                user.Cpf = Console.ReadLine();
+
+                Console.WriteLine($" Seu Cpf é : {user.Cpf}");
+
+                Console.WriteLine("Digite seu email: ");
+                user.Email = Console.ReadLine().Trim();
+
+                Console.WriteLine($"seu email é: {user.Email}");
+
+
+                }
+                else
+                {
+                    rodando = false;
+                }
+            }
+
+
+        }
+
+        private static int getMenu()
+        {
+            int menu = 0;
+            bool MenuValido = false;
+            while (!MenuValido)
+            {
+
+                Console.WriteLine("Digite 1 Para Cadastrar Usuario ou 2 Para Cancelar Operacao: ");
+
+                string? menutemporario = Console.ReadLine().Trim();
+                if (!string.IsNullOrEmpty(menutemporario))
+                {
+
+                    switch (menutemporario)
+                    {
+                        case "1":
+                            menu = Convert.ToInt32(menutemporario);
+                            MenuValido = true;
+
+                            break;
+
+                        case "2":
+                            menu = Convert.ToInt32(menutemporario);
+                            MenuValido = true;
+                            break;
+                    }
+
+
+                }
+                Console.Clear();
+
+            }
+
+            return menu;
+        }
+    }
+
+
+
+
+}
